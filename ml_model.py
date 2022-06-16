c0160afc7bb9864dc26b3fe1e5ad2266d236b4ca
VSANthosh-k <vsk438@gmail.com>
2022-06-16 22:11:36 +0530

    added 3 new ml model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,15 @@
 print("adding all ML related code here")
+
+
+def model1():
+    print("adding code for ML model")
+
+
+def model2():
+    print("adding code for ML model")
+
+
+def model3():
+    print("adding code for ML model")
+
+
